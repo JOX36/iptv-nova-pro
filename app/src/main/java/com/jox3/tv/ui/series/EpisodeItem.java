b417cc30c1669f009726f686223ac3cfe6efa1dc
JOX36 <angelvalenciamurcielago@gmail.com>
2026-04-23 21:29:47 -0500

    Create EpisodeItem.java

---
+++ app/src/main/java/com/jox3/tv/ui/series/EpisodeItem.java
@@ -0,0 +1,8 @@
+package com.jox3.tv.ui.series;
+
+public class EpisodeItem {
+    public String id, title, url, plot;
+    public EpisodeItem(String id, String title, String url, String plot) {
+        this.id = id; this.title = title; this.url = url; this.plot = plot;
+    }
+}
