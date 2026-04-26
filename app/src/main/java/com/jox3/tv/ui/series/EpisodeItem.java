76d4e67806fa70751f8e2cd5bb3cc90b8598e22b
JOX36 <angelvalenciamurcielago@gmail.com>
2026-04-26 11:30:13 -0500

    Add files via upload

--- app/src/main/java/com/jox3/tv/ui/series/EpisodeItem.java
+++ app/src/main/java/com/jox3/tv/ui/series/EpisodeItem.java
@@ -1,8 +1,13 @@
 package com.jox3.tv.ui.series;
 
 public class EpisodeItem {
-    public String id, title, url, plot;
-    public EpisodeItem(String id, String title, String url, String plot) {
-        this.id = id; this.title = title; this.url = url; this.plot = plot;
+    public String id, title, url, plot, thumb;
+
+    public EpisodeItem(String id, String title, String url, String plot, String thumb) {
+        this.id    = id;
+        this.title = title;
+        this.url   = url;
+        this.plot  = plot;
+        this.thumb = thumb;
     }
 }
