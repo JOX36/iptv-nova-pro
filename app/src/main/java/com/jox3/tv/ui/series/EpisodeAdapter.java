6cb275964483f5b5f5fcd325393b3b077fe68c56
JOX36 <angelvalenciamurcielago@gmail.com>
2026-04-26 11:29:36 -0500

    Add files via upload

--- app/src/main/java/com/jox3/tv/ui/series/EpisodeAdapter.java
+++ app/src/main/java/com/jox3/tv/ui/series/EpisodeAdapter.java
@@ -3,11 +3,13 @@ package com.jox3.tv.ui.series;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.jox3.tv.R;
 
 import java.util.List;
@@ -20,7 +22,8 @@ public class EpisodeAdapter extends RecyclerView.Adapter<EpisodeAdapter.VH> {
     private final OnClick listener;
 
     public EpisodeAdapter(List<EpisodeItem> items, OnClick l) {
-        this.items = items; listener = l;
+        this.items = items;
+        listener = l;
     }
 
     @NonNull @Override
@@ -34,19 +37,43 @@ public class EpisodeAdapter extends RecyclerView.Adapter<EpisodeAdapter.VH> {
     public void onBindViewHolder(@NonNull VH h, int pos) {
         EpisodeItem ep = items.get(pos);
         h.tvTitle.setText(ep.title);
-        h.tvPlot.setText(ep.plot);
-        h.tvPlot.setVisibility(ep.plot.isEmpty() ? View.GONE : View.VISIBLE);
+
+        if (ep.plot != null && !ep.plot.isEmpty()) {
+            h.tvPlot.setText(ep.plot);
+            h.tvPlot.setVisibility(View.VISIBLE);
+        } else {
+            h.tvPlot.setVisibility(View.GONE);
+        }
+
+        // Miniatura
+        if (ep.thumb != null && !ep.thumb.isEmpty()) {
+            h.ivThumb.setVisibility(View.VISIBLE);
+            h.tvThumbPh.setVisibility(View.GONE);
+            Glide.with(h.itemView.getContext().getApplicationContext())
+                .load(ep.thumb)
+                .centerCrop()
+                .placeholder(android.R.color.darker_gray)
+                .into(h.ivThumb);
+        } else {
+            h.ivThumb.setVisibility(View.GONE);
+            h.tvThumbPh.setVisibility(View.VISIBLE);
+        }
+
         h.itemView.setOnClickListener(v -> listener.onClick(ep));
     }
 
     @Override public int getItemCount() { return items.size(); }
 
     static class VH extends RecyclerView.ViewHolder {
-        TextView tvTitle, tvPlot;
+        ImageView ivThumb;
+        TextView tvTitle, tvPlot, tvThumbPh;
+
         VH(View v) {
             super(v);
-            tvTitle = v.findViewById(R.id.tv_title);
-            tvPlot  = v.findViewById(R.id.tv_plot);
+            ivThumb   = v.findViewById(R.id.iv_thumb);
+            tvThumbPh = v.findViewById(R.id.tv_thumb_ph);
+            tvTitle   = v.findViewById(R.id.tv_title);
+            tvPlot    = v.findViewById(R.id.tv_plot);
         }
     }
 }
