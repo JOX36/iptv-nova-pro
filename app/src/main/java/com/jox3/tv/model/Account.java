477b5acf3e9bf2d73ba4b0d992f141968516bedc
JOX36 <angelvalenciamurcielago@gmail.com>
2026-04-30 22:13:01 -0500

    Add files via upload

--- app/src/main/java/com/jox3/tv/model/Account.java
+++ app/src/main/java/com/jox3/tv/model/Account.java
@@ -4,6 +4,8 @@ public class Account {
     public String host;
     public String user;
     public String pass;
+    public String type; // "xtream" o "m3u"
+    public String m3uUrl; // URL completa M3U si es M3U
 
     public Account() {}
 
@@ -11,11 +13,16 @@ public class Account {
         this.host = host;
         this.user = user;
         this.pass = pass;
+        this.type = "xtream";
     }
+
+    public boolean isM3u() { return "m3u".equals(type); }
 
     public String key() { return host + "|" + user; }
 
     public String displayHost() {
+        if (isM3u() && m3uUrl != null)
+            return m3uUrl.replaceAll("^https?://", "").split("[:/]")[0];
         return host.replaceAll("^https?://", "").split(":")[0];
     }
 }
