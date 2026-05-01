5c8e7bd26e4121c4f08353e880424c21753157f8
JOX36 <angelvalenciamurcielago@gmail.com>
2026-04-30 22:16:36 -0500

    Add files via upload

--- app/src/main/java/com/jox3/tv/util/AppState.java
+++ app/src/main/java/com/jox3/tv/util/AppState.java
@@ -1,5 +1,6 @@
 package com.jox3.tv.util;
 
+import com.jox3.tv.api.M3uParser;
 import com.jox3.tv.api.XtreamApi;
 import com.jox3.tv.model.Account;
 import com.jox3.tv.model.Category;
@@ -20,7 +21,9 @@ public class AppState {
     public List<Category> vodCats    = new ArrayList<>();
     public List<Category> seriesCats = new ArrayList<>();
 
-    // Canal actual y lista para navegación
+    // Datos M3U indexados por categoría
+    public M3uParser.ParseResult m3uData = null;
+
     public MediaItem current;
     public List<MediaItem> channelList = new ArrayList<>();
     public int channelIdx = -1;
@@ -31,6 +34,45 @@ public class AppState {
         liveCats.clear();
         vodCats.clear();
         seriesCats.clear();
+        m3uData = null;
+    }
+
+    public void setM3uData(M3uParser.ParseResult data) {
+        m3uData = data;
+        // Poblar las listas de categorías desde M3U
+        liveCats.clear();
+        vodCats.clear();
+        seriesCats.clear();
+        liveCats.addAll(data.liveCats);
+        vodCats.addAll(data.vodCats);
+        seriesCats.addAll(data.seriesCats);
+
+        // Marcar items como cargados
+        for (Category c : liveCats) {
+            String group = c.name;
+            if (data.liveItems.containsKey(group)) {
+                c.items = data.liveItems.get(group);
+                c.loaded = true;
+            }
+        }
+        for (Category c : vodCats) {
+            String group = c.name;
+            if (data.vodItems.containsKey(group)) {
+                c.items = data.vodItems.get(group);
+                c.loaded = true;
+            }
+        }
+        for (Category c : seriesCats) {
+            String group = c.name;
+            if (data.seriesItems.containsKey(group)) {
+                c.items = data.seriesItems.get(group);
+                c.loaded = true;
+            }
+        }
+    }
+
+    public boolean isM3u() {
+        return account != null && account.isM3u();
     }
 
     public List<Category> cats(String type) {
