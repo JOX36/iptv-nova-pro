a1517dbe4b1b41a53240ff45fa295772ed76277f
JOX36 <angelvalenciamurcielago@gmail.com>
2026-06-19 12:07:18 -0500

    Upgrade compileSdk and targetSdk to version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.jox3.tv"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jox3.tv"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
